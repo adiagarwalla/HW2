dd1f1ddd9b77c7ddc9841377025e6581d05acf6a
Collin Stedman <cstedman@princeton>
2015-03-30 18:21:33 -0400

    OMG IT WOARKS

--- columns.py
+++ columns.py
@@ -1,26 +1,49 @@
+import linecache
 import numpy as np
 from sklearn import linear_model
 
-file_sample = "../intersected_final_chr1_cutoff_20_sample.bed"
-file_test = "../intersected_final_chr1_cutoff_20_test.bed"
-file_train = "../intersected_final_chr1_cutoff_20_train_revised.bed"
-file_bed = "../intersected_final_chr1_cutoff_20.bed"
+file_sample = "data/intersected_final_chr1_cutoff_20_sample.bed"
+file_test = "data/intersected_final_chr1_cutoff_20_test.bed"
+file_train = "data/intersected_final_chr1_cutoff_20_train_revised.bed"
+file_bed = "data/intersected_final_chr1_cutoff_20.bed"
 
 # Get non-NAN sites from sample file
 def read_sample_nonNan():
-	print "Getting started"
-	f_train = open(file_train)
-	test = []
-	with open(file_sample) as f:
-		for i, line in enumerate(f, 1):
-			output_line = line.strip().split()
-			if (output_line[5] == '1'):
-				clf = linear_model.LinearRegression()
-
-
-	print test
+    print "Getting started"
+    f_train = open(file_train)
+    X = []
+    Y = []
+    with open(file_sample) as f:
+        for i, line in enumerate(f, 1):
+            sample = line.strip().split()
+            if (sample[4] != 'nan'):
+                Y.append(sample[4])
+                row = linecache.getline(file_train, i).strip().split()
+                train_data = row[4: -1]
+                total = 0
+                count = 0
+                for j in train_data:
+                    if j != 'nan':
+                        total = total + float(j)
+                        count = count + 1
+                avg = total / float(count)
+                train_data2 = []
+                for j in train_data:
+                    if j == 'nan':
+                        train_data2.append(avg)
+                    else:
+                        train_data2.append(float(j))
+                X.append(train_data2)
+    print X[0:10]
+                
+    # XMatrix = np.array(X)
+    # YMatrix = np.array(Y)
+    
+    clf = linear_model.LinearRegression()
+    clf.fit(np.array(X, 'float_'), np.array(Y, 'float_'))
+    print clf.coef_
 
 def main():
-	read_sample_nonNan()
+    read_sample_nonNan()
 
 main()
