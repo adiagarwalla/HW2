67d79643a5443d27c9de4b5aa92b99a4ecc46d28
adiagarwalla <aa4@princeton.edu>
2015-03-30 17:24:27 -0400

    Moving onto GLM

--- columns.py
+++ columns.py
@@ -1,3 +1,6 @@
+import numpy as np
+from sklearn import linear_model
+
 file_sample = "../intersected_final_chr1_cutoff_20_sample.bed"
 file_test = "../intersected_final_chr1_cutoff_20_test.bed"
 file_train = "../intersected_final_chr1_cutoff_20_train_revised.bed"
@@ -6,9 +9,14 @@ file_bed = "../intersected_final_chr1_cutoff_20.bed"
 # Get non-NAN sites from sample file
 def read_sample_nonNan():
 	print "Getting started"
+	f_train = open(file_train)
 	test = []
 	with open(file_sample) as f:
-		test = f.readLines()
+		for i, line in enumerate(f, 1):
+			output_line = line.strip().split()
+			if (output_line[5] == '1'):
+				clf = linear_model.LinearRegression()
+
 
 	print test
 
@@ -16,6 +24,3 @@ def main():
 	read_sample_nonNan()
 
 main()
-
-
-    
