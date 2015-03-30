af7f2858405a53fc98581b0c436681527a9c77c9
adiagarwalla <aa4@princeton.edu>
2015-03-30 16:15:22 -0400

    Stuck

--- columns.py
+++ columns.py
@@ -1,2 +1,21 @@
+file_sample = "../intersected_final_chr1_cutoff_20_sample.bed"
+file_test = "../intersected_final_chr1_cutoff_20_test.bed"
+file_train = "../intersected_final_chr1_cutoff_20_train_revised.bed"
+file_bed = "../intersected_final_chr1_cutoff_20.bed"
+
 # Get non-NAN sites from sample file
 def read_sample_nonNan():
+	print "Getting started"
+	test = []
+	with open(file_sample) as f:
+		test = f.readLines()
+
+	print test
+
+def main():
+	read_sample_nonNan()
+
+main()
+
+
+    
