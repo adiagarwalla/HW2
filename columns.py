7744309dedefe5520c6d661179d65d8ee5acd9e3
Collin Stedman <cstedman@princeton>
2015-03-30 20:59:50 -0400

    First one down

--- columns.py
+++ columns.py
@@ -1,3 +1,4 @@
+import math
 import linecache
 import numpy as np
 from sklearn import linear_model
@@ -11,7 +12,8 @@ file_bed = "data/intersected_final_chr1_cutoff_20.bed"
 def read_sample_nonNan():
     print "Getting started"
     f_train = open(file_train)
-    X = []
+    X_train = []
+    X_test = []
     Y = []
     with open(file_sample) as f:
         for i, line in enumerate(f, 1):
@@ -33,31 +35,55 @@ def read_sample_nonNan():
                         train_data2.append(avg)
                     else:
                         train_data2.append(float(j))
-                X.append(train_data2)
-    print X[0:10]
+                X_train.append(train_data2)
+            elif (sample[5] == '0'):
+                row = linecache.getline(file_train, i).strip().split()
+                train_data = row[4: -1]
+                total = 0
+                count = 0
+                for j in train_data:
+                    if j != 'nan':
+                        total = total + float(j)
+                        count = count + 1
+                avg = total / float(count)
+                train_data2 = []
+                for j in train_data:
+                    if j == 'nan':
+                        train_data2.append(avg)
+                    else:
+                        train_data2.append(float(j))
+                X_test.append(train_data2)
                 
-    # XMatrix = np.array(X)
-    # YMatrix = np.array(Y)
-    
     clf = linear_model.LinearRegression()
-    clf.fit(np.array(X, 'float_'), np.array(Y, 'float_'))
+    clf.fit(np.array(X_train, 'float_'), np.array(Y, 'float_'))
     print clf.coef_
 
     Y_test = []
 
+    X_test_final = []
+    Y_test_final = []
+
+
     with open(file_test) as f_test:
-    	for k, line_test in enumerate(f_test, 1):
-    		test_sample = line_test.strip().split()
-    		if (test_sample[5] == '0'):
-    			Y_test.append(test_sample[5])
+        for k, line_test in enumerate(f_test, 1):
+            test_sample = line_test.strip().split()
+            if (test_sample[5] == '0'):
+                Y_test.append(test_sample[4])
+
+    for i, j in enumerate(Y_test):
+        if j != 'nan':
+            Y_test_final.append(j)
+            X_test_final.append(X_test[i])
+
+    print len(X_test_final)
+    print len(Y_test_final)
 
     # The mean square error
-    print("Residual sum of squares: %.2f"
-      % np.mean((clf.predict(np.array(X, 'float_')) - np.array(Y_test, 'float_')) ** 2))
+    rss = np.mean((clf.predict(np.array(X_test_final, 'float_')) - np.array(Y_test_final, 'float_')) ** 2)
+    print("Residual sum of squares: %.5f" % rss)
 
     # The root mean square error
-    print("Residual root sum of squares: %.2f"
-      % math.sqrt(np.mean((clf.predict(np.array(X, 'float_')) - np.array(Y_test, 'float_')) ** 2)))
+    print("Residual root sum of squares: %.5f" % math.sqrt(rss))
 
 def main():
     read_sample_nonNan()
